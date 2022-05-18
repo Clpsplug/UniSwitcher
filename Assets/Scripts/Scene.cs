a778df8fad7682b7234100adf3f1abe62f421c59
Collapsed Plug <hsp.tosh.5200113@gmail.com>
2022-05-18 20:54:19 +0900

    Add base implementation of IScene for easy access

--- Assets/Scripts/Scene.cs
+++ Assets/Scripts/Scene.cs
@@ -1,20 +1,10 @@
 ﻿using UniSwitcher.Domain;
 
-public class Scene : IScene
+public class Scene : BaseScene
 {
     public static Scene FirstScene => new Scene("Assets/Scenes/SampleScene.unity");
-
     public static Scene SecondScene => new Scene("Assets/Scenes/SecondScene.unity");
-    
-    private readonly string _rawValue;
 
-    public Scene(string rawValue)
-    {
-        _rawValue = rawValue;
-    }
-    
-    public string GetRawValue()
-    {
-        return _rawValue;
-    }
+    private Scene(string rawValue) : base(rawValue)
+    { }
 }
