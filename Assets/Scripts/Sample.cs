aae201bfc88fc0326f45f2666fe5bde2db108268
Collapsed Plug <hsp.tosh.5200113@gmail.com>
2022-07-28 00:49:36 +0900

    Remove analytics package by default from the sample project

--- Assets/Scripts/Sample.cs
+++ Assets/Scripts/Sample.cs
@@ -1,19 +1,23 @@
 ﻿using Cysharp.Threading.Tasks;
 using UniSwitcher;
-//using Unity.Services.Core;
-//using Unity.Services.Analytics;
+#if UGS_ANALYTICS
+using Unity.Services.Core;
+using Unity.Services.Analytics;
+#endif
 using UnityEngine;
 
 public class Sample : Switcher
 {
     private async void Awake()
     {
-        /*
+        // This part is initially disabled to prevent accidental use of your Unity Gaming Service Analytics event quota.
+        // Include com.unity.services.analytics into this sample project to enable this part of the code.
+#if UGS_ANALYTICS
         try
         {
-           Debug.Log("Init Unity Services");
-           var options = new InitializationOptions();
-           options.SetOption("com.unity.services.core.environment-name", "dev");
+            Debug.Log("Init Unity Services");
+            var options = new InitializationOptions();
+            options.SetOption("com.unity.services.core.environment-name", "dev");
             await UnityServices.InitializeAsync(options);
             var consentIdentifiers = await AnalyticsService.Instance.CheckForRequiredConsents();
             Debug.Log("OK");
@@ -22,10 +26,10 @@ public class Sample : Switcher
         {
             // Something went wrong when checking the GeoIP, check the e.Reason and handle appropriately.
             Debug.LogWarning($"Oops! Consent failed. {e.Reason}");
-        }*/
+        }
+#endif
     }
-    
-    
+
     private void Start()
     {
         PerformSceneTransition(
--- Assets/Scripts/Scene.cs
+++ Assets/Scripts/Scene.cs
@@ -1,11 +1,24 @@
 ﻿using UniSwitcher.Domain;
 
 public class Scene : BaseScene
+#if UGS_ANALYTICS
+    , IReportable
+#endif
 {
     public static Scene FirstScene => new Scene("Assets/Scenes/SampleScene.unity");
     public static Scene SecondScene => new Scene("Assets/Scenes/SecondScene.unity");
+
+    // This part is initially disabled to prevent accidental use of your Unity Gaming Service Analytics event quota.
+    // Include com.unity.services.analytics into this sample project to enable this part of the code.
+#if UGS_ANALYTICS
     public override string ScreenVisitEventName => "screenVisit";
     public override string ScreenVisitEventPropertyName => "screenName";
+    public bool DoNotReport()
+    {
+        return false;
+    }
+#endif
+    // The part above is disabled until you manually include com.unity.services.analytics into this sample project.
 
     private Scene(string rawValue) : base(rawValue)
     { }
